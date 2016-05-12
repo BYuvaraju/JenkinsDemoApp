7f022406c6530ee5d9398a8389de4459b54f6805
Yuvaraju <boligorla.yuvaraju@gmail.com>
2016-05-12 16:54:18 +0530

    Initial commit

---
+++ app/src/main/java/com/melorra/jenkinsdemoapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.melorra.jenkinsdemoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
