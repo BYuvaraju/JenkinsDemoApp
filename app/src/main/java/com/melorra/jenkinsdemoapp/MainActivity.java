b3f08cb9db91476cdc591f4534edfdf3234a9af1
Yuvaraju <boligorla.yuvaraju@gmail.com>
2016-05-23 14:33:11 +0530

    first commit

--- app/src/main/java/com/melorra/jenkinsdemoapp/MainActivity.java
+++ app/src/main/java/com/melorra/jenkinsdemoapp/MainActivity.java
@@ -2,14 +2,17 @@ package com.melorra.jenkinsdemoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Test", Toast.LENGTH_LONG).show();
 
     }
 }
